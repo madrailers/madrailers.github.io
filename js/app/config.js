7100deeecef9a5c39b85e1c0b48075fdffff7740
Zachery Moneypenny <whazzmaster@gmail.com>
2014-02-11 15:34:27 -0600

    Remove old site implementation

--- js/app/config.js
+++
@@ -1,7 +0,0 @@
-define([], function() {
-  var Config = {
-    MeetupUrl: "http://api.meetup.com/2/events?group_id=1747892&status=upcoming&order=time&limited_events=False&desc=false&offset=0&format=json&page=20&fields=&sig_id=3013436&sig=657f5edab6dc03d61e955c8b79e8a564494d403a",
-    YouTubeUrl: "https://gdata.youtube.com/feeds/api/videos?author=pxYqZzpZNIA98Ps6hKJuUw&max-results=5&v=2&alt=jsonc&orderby=published"
-  }
-  return Config;
-});
